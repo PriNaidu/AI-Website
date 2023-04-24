fd52ffa4753ece0a64bbc2824f3b04bfc161c7a8
Himanshu Devgan <himanshudevgan@Himanshus-MacBook-Pro.local>
2023-04-24 23:21:34 +0530

    yml added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     assetPrefix: '/ai-bot-website/',
-   basePath: '/ai-bot-website',
+  //  basePath: '/ai-bot-website',
    trailingSlash: true
 }
 
