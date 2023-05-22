3a6f1ccbb7fb29c724199bda6ba7f182b27c54d2
Lalit Sahu <lalit.sahu@venturepact.com>
2023-05-22 11:56:52 +0530

    Pricing section

--- pages/Components/Pricing.tsx
+++ pages/Components/Pricing.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from "react";
-import "./AIPricing.css"
+// import "./AIPricing.css"
 import { features } from "process";
 import AIPlan1 from "./Assets/AIPlan1";
 import AIPlan2 from "./Assets/AIPlan2";
@@ -11,7 +11,8 @@ const Pricing = () => {
       image: <AIPlan1 />,
       title: "Free",
       desc: "Good superb content for Advanced users  Advanced users",
-      price: "$0",
+      monthly: "$0",
+      yearly: "$0",
       button: "Free Plan",
       features:["01 bot", "35 msg per day / 420 msg per month","File upload limit - 04 MB"]
     },
@@ -20,7 +21,8 @@ const Pricing = () => {
       image: <AIPlan2 />,
       title: "Basic",
       desc: "Good superb content for Advanced users  Advanced users",
-      price: "$22",
+      monthly: "$22",
+      yearly: "$14",
       button: "Upgrade Plan",
       features:["04 bots", "2500 messages per month","File upload limit - 27 MB"]
     },
@@ -28,7 +30,8 @@ const Pricing = () => {
       image: <AIPlan3 />,
       title: "Standard",
       desc: "Good superb content for Advanced users  Advanced users",
-      price: "$55",
+      monthly: "$55",
+      yearly: "$35",
       button: "Upgrade Plan",
       features:["18 bot", "4500 messages per month","File upload limit - 45 MB / 3.5M characters","Add your OpenAI key","100 links"]
     },
@@ -36,25 +39,47 @@ const Pricing = () => {
       image: <AIPlan4 />,
       title: "Advanced", 
       desc: "Good superb content for Advanced users  Advanced users",
-      price: "$115",
+      monthly: "$115",
+      yearly: "$95",
       button: "Upgrade Plan",
       features:["30 bot", "12500 messages per month","File upload limit - 55 MB / 4.5M characters","Add your OpenAI key","1000 links"]
     },
   ];
 
 const[active, SetActive] = useState(3);
-
+const [checked, setChecked] = useState(false)
   return (
     <Fragment>
+      <div className="ai-plan-landing">
+      <div className="pricing-head">
+               <h2>Choose the best plan for your business </h2>
+
+               <div className="pricing-toggle">
+                 <span className={checked ? "monthly-label" : "monthly-label selected"}>Monthly</span>
+                 <label
+                   className="pricing-toggle-wrapper"
+                   onChange={() => {
+                     setChecked(!checked);
+                   }}
+                 >
+                   <input type="checkbox" checked={checked} />
+                   <span className="toggle-switch round"></span>
+                 </label>
+                 <span className={checked ? "yearly-label selected" : "yearly-label"}>Yearly</span>
+               </div>
+               <p>(Save 2 months with annual billing)</p>
+             </div>
       <div className="ai-plan-flex">
         {data.map((plans , i) => (
           <div className={`aiplan-bx ${active === i ? "active-aiplan" : ""}`} key={i} onClick={()=>SetActive(i)}>
             <div className="aiplan-ing-bx">{plans.image}</div>
             <div className="aiplan-content-bx">
-              <h2>{plans.title}{plans.title==="Advanced" && <span className="ai-recommend">Recommended</span>} {plans.title==="Standard" && <span className="ai-current-plan">Current Plan</span>}</h2>
+              <h2>{plans.title}{plans.title==="Advanced" && <span className="ai-recommend">Recommended</span>} 
+              {/* {plans.title==="Standard" && <span className="ai-current-plan">Current Plan</span>} */}
+              </h2>
               <p>{plans.desc}</p>
               <div className="ai-plan-price fx jst-sp-bt">
-                <span>{plans.price}</span> per month
+                <span>{checked ? plans.yearly: plans.monthly}</span> per month
               </div>
               <button>{plans.button}</button>
               <ul>
@@ -71,6 +96,7 @@ const[active, SetActive] = useState(3);
             </div>
           </div>
         ))}
+      </div>
       </div>
     </Fragment>
   );
