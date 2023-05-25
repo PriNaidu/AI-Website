65d37b75922183bba1530da8219e487b5c6c29a3
priyanka naidu <priyanka.naidu@venturepact.com>
2023-05-25 13:42:07 +0530

    [Fixes]

--- pages/Components/LaunchChatbot.tsx
+++ pages/Components/LaunchChatbot.tsx
@@ -40,7 +40,7 @@ const LaunchChatbot = () => {
             <h3>
               Launch your first chatbot in <span>minutes!</span>
             </h3>
-            <button>Try free</button>
+            <button>Try for free</button>
           </div>
           <div className="launchbot-options">
             <h5>Frequently asked questions</h5>
