950d630552b6f2df3a43594db944cee2780be869
priyanka naidu <priyanka.naidu@venturepact.com>
2023-06-13 18:18:41 +0530

    [Changes]

--- pages/Components/BannerSection.tsx
+++ pages/Components/BannerSection.tsx
@@ -8,9 +8,9 @@ const BannerSection = () => {
 
   const handleSignup = (e: any) => {
     e.preventDefault();
-    if (!userEmail) {
-      return;
-    }
+    // if (!userEmail) {
+    //   return;
+    // }
     window.location.replace(url);
   };
   return (
