762c3804707aa1d1a4f42ef10e33d7ef58d70a1c
tamana1212 <tamanasharma2023@gmail.com>
2023-09-29 10:11:17 +0530

    sub-footer fixes

--- pages/Components/Assets/SubFooter.tsx
+++ pages/Components/Assets/SubFooter.tsx
@@ -2,8 +2,8 @@ const SubFooter = () => {
     return (
         <div className="sub-footer">
         <div className="sub-footer-container">
-          <span className="left-text">©2022, All Rights Reserved</span>
-          <span className="right-text">© VPX Projects MT LLC</span>
+          <span className="left-text">©2023, All Rights Reserved</span>
+          {/* <span className="right-text">© VPX Projects MT LLC</span> */}
         </div>
       </div>
      )
