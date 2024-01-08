16f8bf5e54d8836a0d1e806fcf912546520c110b
siddharth.tripathi.vp <siddharth.tripathi@venturepact.com>
2024-01-08 12:36:52 +0530

    Updated favicon, meta title and meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,11 +11,13 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>ChatBot Builder</title>
-        <link rel="shortcut icon" href="chatbot-builder-favicon.png" />
+        <link rel="shortcut icon" href="https://dlvkyia8i4zmz.cloudfront.net/UCNdkpFbQt60Zz6fLKe2_CBB_favicon.svg" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&family=Outfit&family=Poppins:ital@0;1&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
         />
+        <meta name="title" content="Chatbot Builder | Design and Deploy AI Chatbots without Coding"></meta>
+        <meta name="description" content="ChatBot Builder | Build AI chatbots trained on your docs, chats & URLs. Automate support, sales, & FAQs."></meta>
       </Head>
       <Component {...pageProps} />
     </>
